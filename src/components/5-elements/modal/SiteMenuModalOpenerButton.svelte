c4630781a5aa31e46d77d0d7f75f3f93ca692e45
Driijko <driijko@gmail.com>
2023-09-20 13:00:53 -0400

    SiteMenuModalOpenerButton.

--- src/components/5-elements/modal/SiteMenuModalOpenerButton.svelte
+++ src/components/5-elements/modal/SiteMenuModalOpenerButton.svelte
@@ -2,18 +2,22 @@
 <script>
   // IMPORTS ----------------------------------------------------
   import { modals, interfaceModal } from "../../../dynamic/modals";
+  import { setCurrentInterface } from "../../../dynamic/currentInterface";
   import MenuIcon from "../../6-icons/interface/MenuIcon.svelte";
 
   // EVENT HANDLERS ---------------------------------------------
   function handleClick() {
-
-  }
+    if ($interfaceModal === false) {
+      modals.open("interfaceModal")
+    }
+    setCurrentInterface("siteMenu");
+  };
 
 </script>
 
 <!-- MARKUP /////////////////////////////////////////// -->
 <button type="button" class="site-menu-modal-opener-button" 
-  on:click={()=>modals.open("siteMenu")}
+  on:click={handleClick}
   title="open menu"
 >
   <MenuIcon />

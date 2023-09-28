3c222f505f6e2ab333acfa93e16088e4b365f40c
Driijko <driijko@gmail.com>
2023-09-28 10:04:06 -0400

    SiteMenuTabTransition: adjust for large-desktop mode.

--- src/components/1-site/interface/site-menu/tab-system/SiteMenuTabTransition.svelte
+++ src/components/1-site/interface/site-menu/tab-system/SiteMenuTabTransition.svelte
@@ -3,12 +3,24 @@
   // IMPORTS ----------------------------------
   import shift from "../../../../../scripts/transitions/shift";
   import { linear } from "svelte/easing";
+  import { breakpoint } from "../../../../../dynamic/breakpoint";
+  import { layout } from "../../../../../static/siteSettings";
   import { siteMenuGetTabPosition, siteMenuCurrentTab, siteMenuPrevTab } 
   from "../../../../../dynamic/siteMenuTabs";
+
+  // DESTRUCTURING ---------------------------------
+  const { interfaceAreaWidth } = layout;
 
   // REACTIVE -----------------------------------
   $: currentTabPosition = siteMenuGetTabPosition($siteMenuCurrentTab);
   $: prevTabPosition = siteMenuGetTabPosition($siteMenuPrevTab);
+
+  let distance = window.innerWidth;
+  $: if ($breakpoint === "large-desktop") {
+    distance = window.innerWidth * (interfaceAreaWidth * 0.01);
+  } else {
+    distance = window.innerWidth;
+  }
 
 </script>
 
@@ -16,14 +28,14 @@
 <div class="site-menu-tab fill"
   in:shift="{{
     x: currentTabPosition < prevTabPosition ? 
-      -window.innerWidth : window.innerWidth, 
+      -distance : distance, 
     duration: 300, 
     delay: 300,
     easing: linear,
   }}"
   out:shift="{{
     x: currentTabPosition < prevTabPosition ? 
-      window.innerWidth: -window.innerWidth, 
+      distance: -distance, 
     duration: 300,
     easing: linear,
   }}"

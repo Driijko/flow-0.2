89d9a3a9c512fb83b585c1af795a371e7e91cfe3
Driijko <driijko@gmail.com>
2023-10-08 13:24:39 -0400

    Move background audio specific code in OpeningRouter into site-specific folder.

--- src/components/1-site/Routers/OpeningRouter.svelte
+++ src/components/1-site/Routers/OpeningRouter.svelte
@@ -3,18 +3,14 @@
   // IMPORTS --------------------------------------------------
   import { currentPage } from "../../../dynamic/currentPage";
   import LoadingPage from "../../2-pages/LoadingPage.svelte";
-  import OpeningPromptPage from "../../../site-specific/background-audio/components/2-pages/OpeningPromptPage.svelte";
-  import AudioBkg from "../../../components-site-specific/background-audio/components/1-site/AudioBkg.svelte";
   import Breakpoint from "../layout/Breakpoint.svelte";
 
 </script>
 
 <!-- MARKUP ///////////////////////////////////////////////// -->
-<AudioBkg />
 {#if $currentPage === "loading"}
   <LoadingPage />
-{:else if $currentPage === "opening-prompt"}
-  <OpeningPromptPage />
+
 {:else}
   <Breakpoint />
 {/if}
---
+++ src/site-specific/background-audio/components/1-site/routers/OpeningRouter+.svelte
@@ -0,0 +1,14 @@
+<!-- SCRIPTS //////////////////////////////////////////// -->
+<script>
+  // IMPORTS -------------------------------------------------------
+  import OpeningPromptPage from "../../../site-specific/background-audio/components/2-pages/OpeningPromptPage.svelte";
+  import AudioBkg from "../../../components-site-specific/background-audio/components/1-site/AudioBkg.svelte";
+
+</script>
+
+<!-- MARKUP /////////////////////////////////////////// -->
+<!-- Place at the top of markup of Opening Router -->
+<AudioBkg />
+
+{:else if $currentPage === "opening-prompt"}
+  <OpeningPromptPage />
